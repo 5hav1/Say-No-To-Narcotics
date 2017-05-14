6bb3bbb0610ab6cafd433f9dbbc7a9cbca665317
Shavindra Wickramathilaka <shaviwic9@gmail.com>
2017-05-14 12:33:26 +0530

    Add files via upload

---
+++ template.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+    <title>Starter Template - Materialize</title>
+
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="css/main.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  </head>
+
+  <body>
+    <nav class="teal lighten-2">
+      <div class="nav-wrapper">
+        <a href="#!" class="brand-logo">Say No To Narcotics</a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="sass.html"><i class="material-icons">perm_identity</i></a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="content">
+      <div class="row">
+        <div class="col m2 side-navigation">
+            <!-- <div class="side-navigation"> -->
+                <h4>“Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br>
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”</h4>
+
+            <!-- </div> -->
+            <!-- <img src="images/home/slider/slide1/middle.png" class="img-responsive slider-house" alt="slider image">
+            <img src="images/home/slider/slide1/title.png" class="slider-sun" alt="slider image">
+            <img src="images/home/slider/slide1/car.png" class="slider-cycle" alt=""> -->
+        </div>
+        <div class="col m10 content-section">
+          <!-- <div> -->
+            <!-- <div class="content-section"> -->
+                <h4>“Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br>
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”</h4>
+
+                <a href="#" class="btn" onclick="openNav4()">SIGN UP</a>
+                <a href="#" class="btn" onclick="openNav5()">LOG IN</a>
+            <!-- </div> -->
+          <!-- </div> -->
+        </div>
+      </div>
+
+    <!--  Scripts-->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="js/materialize.js"></script>
+    <script src="js/main.js"></script>
+  </body>
